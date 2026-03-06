35fe3fe23303e8328389e355873c4b332d2a0a4e
ansh patel <ansh.patel.cg@gmail.com>
2026-03-06 15:59:59 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 06/03/2026, 16:00:00
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + 2*t;
+};
