fd218aa5ebdb49ae0ee8d625ea36478cac6a07bd
ansh patel <ansh.patel.cg@gmail.com>
2026-03-06 16:00:15 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.js
@@ -0,0 +1,18 @@
+// Last updated: 06/03/2026, 16:00:15
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;    
+};
