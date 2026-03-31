ee8fb33abd9b89d22a8b41abb930e453186d277e
ansh patel <ansh.patel.cg@gmail.com>
2026-03-31 23:06:17 +0530

    File updated at 31/03/2026, 23:06:17

--- 1-TwoSum/1-TwoSum.js
+++ 1-TwoSum/1-TwoSum.js
@@ -1,15 +1,16 @@
-// Last updated: 06/03/2026, 16:00:17
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-};
+// Last updated: 31/03/2026, 23:06:17
+1/**
+2 * @param {number[]} nums
+3 * @param {number} target
+4 * @return {number[]}
+5 */
+6var twoSum = function(nums, target) {
+7    for (let i = 0; i < nums.length; i++) {
+8        for (let j = i + 1; j < nums.length; j++) {
+9            if (nums[i] + nums[j] === target) {
+10                return [i, j];
+11            }
+12        }
+13    }
+14};
+15
