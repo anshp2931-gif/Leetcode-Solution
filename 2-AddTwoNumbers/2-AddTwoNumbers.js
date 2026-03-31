b97c46dc9930656e91172862f43fc82e466fc5dd
ansh patel <ansh.patel.cg@gmail.com>
2026-03-31 23:19:18 +0530

    File updated at 31/03/2026, 23:19:19

--- 2-AddTwoNumbers/2-AddTwoNumbers.js
+++ 2-AddTwoNumbers/2-AddTwoNumbers.js
@@ -1,15 +1,16 @@
-// Last updated: 31/03/2026, 23:11:45
+// Last updated: 31/03/2026, 23:19:19
 1/**
 2 * @param {number[]} nums
 3 * @param {number} target
 4 * @return {number[]}
 5 */
 6var twoSum = function(nums, target) {
-7    for (let i=0;i<nums.length ; i++){
-8        for(let j = i+1 ; j<nums.length; j++){
-9            if(nums[i] + nums[j] === target)
+7   for (let i=0 ; i<nums.length; i++){
+8    for (let j=i+1 ; j<nums.length ;j++){
+9        if(nums[i]+nums[j]===target){
 10            return [i,j];
 11        }
 12    }
-13};
-14
+13   }
+14};
+15
