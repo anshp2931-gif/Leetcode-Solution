ef220c04e9f0db160f4ef1082c72d8db3bcd23e8
ansh patel <ansh.patel.cg@gmail.com>
2026-04-09 10:39:23 +0530

    File updated at 09/04/2026, 10:39:24

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.cpp
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.cpp
@@ -1,4 +1,4 @@
-// Last updated: 09/04/2026, 10:26:11
+// Last updated: 09/04/2026, 10:39:24
 1class Solution {
 2public:
 3    string reverseVowels(string s) {
@@ -6,23 +6,27 @@
 5        int right = s.size() - 1;
 6
 7        while (left < right) {
-8            while (left < right && 
-9                  !(s[left]=='a'||s[left]=='e'||s[left]=='i'||s[left]=='o'||s[left]=='u'||
-10                    s[left]=='A'||s[left]=='E'||s[left]=='I'||s[left]=='O'||s[left]=='U')) {
-11                left++;
-12            }
-13
-14            while (left < right && 
-15                  !(s[right]=='a'||s[right]=='e'||s[right]=='i'||s[right]=='o'||s[right]=='u'||
-16                    s[right]=='A'||s[right]=='E'||s[right]=='I'||s[right]=='O'||s[right]=='U')) {
-17                right--;
-18            }
-19
-20            swap(s[left], s[right]);
-21            left++;
-22            right--;
-23        }
-24
-25        return s;
-26    }
-27};
+8            if (!(s[left] == 'a' || s[left] == 'A' ||
+9                  s[left] == 'e' || s[left] == 'E' ||
+10                  s[left] == 'i' || s[left] == 'I' ||
+11                  s[left] == 'o' || s[left] == 'O' ||
+12                  s[left] == 'u' || s[left] == 'U')) {
+13                ++left;
+14            }
+15            else if (!(s[right] == 'a' || s[right] == 'A' ||
+16                       s[right] == 'e' || s[right] == 'E' ||
+17                       s[right] == 'i' || s[right] == 'I' ||
+18                       s[right] == 'o' || s[right] == 'O' ||
+19                       s[right] == 'u' || s[right] == 'U')) {
+20                --right;
+21            }
+22            else {
+23                swap(s[left], s[right]);
+24                ++left;
+25                --right;
+26            }
+27        }
+28
+29        return s;
+30    }
+31};
