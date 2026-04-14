0126360a3b8f50599b95b77d4e618c4f692182a9
ansh patel <ansh.patel.cg@gmail.com>
2026-04-14 15:58:10 +0530

    File updated at 14/04/2026, 15:58:10

--- 58-LengthofLastWord/58-LengthofLastWord.cpp
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,4 +1,4 @@
-// Last updated: 09/04/2026, 22:35:18
+// Last updated: 14/04/2026, 15:58:10
 1class Solution {
 2public:
 3    int lengthOfLastWord(string s) {
@@ -6,13 +6,14 @@
 5     int i=s.length()-1;
 6     while(i>=0 && s[i]==' '){
 7        i--;
-8     }
+8     }   
 9     int length =0;
 10     while(i>=0 && s[i]!=' '){
 11        length++;
 12        i--;
 13     }
-14     return length;
-15        
-16    }
-17};
+14     return length; 
+15    }
+16
+17
+18};
