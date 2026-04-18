5d44b3ff3f52c131f722c1a3f610d9d36b408ae1
ansh patel <ansh.patel.cg@gmail.com>
2026-04-18 22:31:42 +0530

    Create 2520-CounttheDigitsThatDivideaNumber

---
+++ 2520-CounttheDigitsThatDivideaNumber/2520-CounttheDigitsThatDivideaNumber.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/04/2026, 22:31:43
+1class Solution {
+2public:
+3    int countDigits(int num) {
+4        
+5        int original =num;
+6        int count=0;
+7        while(num>0){
+8            int digit = num %10;
+9            if(original % digit ==0){
+10                count++;
+11            }
+12            num =num /10;
+13        }
+14
+15return count;
+16
+17        
+18    }
+19};
