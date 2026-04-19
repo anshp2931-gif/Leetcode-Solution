938cdfe88d00e657e756d83e47faabcfe6e29d3a
ansh patel <ansh.patel.cg@gmail.com>
2026-04-19 21:23:32 +0530

    File updated at 19/04/2026, 21:23:34

--- 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.cpp
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.cpp
@@ -1,14 +1,14 @@
-// Last updated: 19/04/2026, 21:17:38
+// Last updated: 19/04/2026, 21:23:34
 1class Solution {
 2public:
 3    int smallestEvenMultiple(int n) {
-4      
-5    if ( n%2==0){
+4
+5      if(n%2==0){
 6        return n;
-7    }
-8    else {
-9        return n *2;
-10    }
-11        
+7      }
+8      else{
+9        return n*2;
+10      }
+11     
 12    }
 13};
