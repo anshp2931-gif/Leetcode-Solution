bd092c6d5bbd6370a2256d6a6c665c97185733f8
ansh patel <ansh.patel.cg@gmail.com>
2026-04-19 17:26:43 +0530

    File updated at 19/04/2026, 17:26:45

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.cpp
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.cpp
@@ -1,18 +1,19 @@
-// Last updated: 19/04/2026, 17:01:59
+// Last updated: 19/04/2026, 17:26:45
 1class Solution {
 2public:
 3    vector<int> buildArray(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<int> res(n);
-6        // we make vector name res for size n
+4       
+5        int n =nums.size();
+6        vector<int>res(n);
 7
-8        for (int i = 0; i < n; i++) {
-9            res[i] = nums[nums[i]]; 
-10            // if i=0; nums[0] is 0 and nums[0] is 0. So we get 0 in 0th index
-11            // now if i=1;nums[1] is 2 and nums[2] is 1. So we get 1 in 1st index
-12            // as loop will run 
-13        }
+8        for (int i=0;i <n;i++){
+9            res[i]=nums[nums[i]];
+10          
+11           
+12        }
+13return res;
 14
-15        return res;
-16    }
-17};
+15
+16
+17    }
+18};
