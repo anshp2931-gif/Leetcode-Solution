188de83e75ecd531bea71d618865d38e764641a9
ansh patel <ansh.patel.cg@gmail.com>
2026-04-22 21:28:51 +0530

    File updated at 22/04/2026, 21:28:51

--- 1773-CountItemsMatchingaRule/1773-CountItemsMatchingaRule.cpp
+++ 1773-CountItemsMatchingaRule/1773-CountItemsMatchingaRule.cpp
@@ -1,17 +1,15 @@
-// Last updated: 22/04/2026, 21:18:51
+// Last updated: 22/04/2026, 21:28:51
 1class Solution {
 2public:
-3    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-4       
-5            string s1="" , s2="";
-6
-7            for(string &w : word1){
-8                s1 =s1+w;
-9            }
-10            for(string &w: word2){
-11                s2 =s2+w;
-12            }
-13            return s1==s2;
-14
-15    }
-16};
+3    bool checkTree(TreeNode* root) {
+4       int leftValue =root->left->val;
+5       int rightValue =root->right->val;
+6       int rootValue = root->val;
+7
+8
+9       if(leftValue +rightValue == rootValue){
+10        return true;
+11       }
+12       return false;
+13    }
+14};
