51676ac834c0328434513953c04db354281bbad2
ansh patel <ansh.patel.cg@gmail.com>
2026-04-22 21:18:51 +0530

    File updated at 22/04/2026, 21:18:51

--- 1773-CountItemsMatchingaRule/1773-CountItemsMatchingaRule.cpp
+++ 1773-CountItemsMatchingaRule/1773-CountItemsMatchingaRule.cpp
@@ -1,22 +1,17 @@
-// Last updated: 22/04/2026, 20:59:41
+// Last updated: 22/04/2026, 21:18:51
 1class Solution {
 2public:
-3    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-4        
-5        int index;
-6        if(ruleKey=="type")
-7        index=0;
-8        else if (ruleKey=="color")
-9        index=1;
-10        else index=2;
-11
-12        int count =0;
-13
-14        for(int i=0 ; i<items.size();i++){
-15            if(items[i][index]==ruleValue){
-16                count++;
-17            }
-18        }
-19      return count; 
-20    }
-21};
+3    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+4       
+5            string s1="" , s2="";
+6
+7            for(string &w : word1){
+8                s1 =s1+w;
+9            }
+10            for(string &w: word2){
+11                s2 =s2+w;
+12            }
+13            return s1==s2;
+14
+15    }
+16};
