3626594c41d1954566db289f8fd374426f554a77
ansh patel <ansh.patel.cg@gmail.com>
2026-04-22 20:42:09 +0530

    File updated at 22/04/2026, 20:42:09

--- 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
@@ -1,23 +1,20 @@
-// Last updated: 22/04/2026, 20:16:06
+// Last updated: 22/04/2026, 20:42:09
 1class Solution {
 2public:
 3    int mostWordsFound(vector<string>& sentences) {
-4        int maxSpace = 0;
-5
-6        for (int i = 0; i < sentences.size(); i++) {
-7            int spaceCount = 0;
-8
-9            for (int j = 0; j < sentences[i].length(); j++) {
-10                if (sentences[i][j] == ' ') {
-11                    spaceCount++;
-12                }
-13
-14                if (maxSpace < spaceCount) {
-15                    maxSpace = spaceCount;
-16                }
-17            }
-18        }
-19
-20        return 1 + maxSpace;
-21    }
-22};
+4       
+5        int maxSpace =0;
+6        for(int i=0; i<sentences.size();i++){
+7            int spaceCount =0;
+8            for(int j=0;j<sentences[i].length();j++){
+9                if(sentences[i][j]==' '){
+10                    spaceCount++;
+11                }
+12                if(maxSpace<spaceCount){
+13                    maxSpace =spaceCount;
+14                }
+15            }
+16        }
+17return 1+ maxSpace;
+18    }
+19};
