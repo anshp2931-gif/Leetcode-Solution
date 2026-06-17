496fac018e77789036802626fa3a78b60b83924d
ansh patel <ansh.patel.cg@gmail.com>
2026-06-17 11:59:07 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.cpp
@@ -0,0 +1,18 @@
+// Last updated: 17/06/2026, 11:59:07
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words[i].size(); j++) {
+                if (words[i][j] == x) {
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
